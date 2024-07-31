1a2bc10c2cf927b4192ae426c33706609640a825
jeffrey <125141643+sootysplash@users.noreply.github.com>
2024-07-31 12:45:24 +0400

    1.21 port

--- src/client/java/me/sootysplash/mixin/MinecraftClientMixin.java
+++ src/client/java/me/sootysplash/mixin/MinecraftClientMixin.java
@@ -3,7 +3,6 @@ package me.sootysplash.mixin;
 import me.sootysplash.ConfigOE;
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,13 +11,11 @@ import static me.sootysplash.MainOE.updatedInventory;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
-    @Unique
-    MinecraftClient mc = MinecraftClient.getInstance();
-
-    @Inject(method = "doItemUse", at = @At("TAIL"))
-    private void onPostItemUse(CallbackInfo ci) {
+    @Inject(method = "handleInputEvents", at = @At("HEAD"))
+    private void onPostInputs(CallbackInfo ci) {
+        MinecraftClient mc = MinecraftClient.getInstance();
         if(mc != null && mc.player != null) {
-            if (mc.player.getItemUseTimeLeft() <= 0 && mc.options.useKey.isPressed() && updatedInventory && ConfigOE.getInstance().enabled) {
+            if (!mc.player.getActiveItem().isEmpty() && mc.player.getItemUseTimeLeft() <= 0 && mc.options.useKey.isPressed() && updatedInventory && ConfigOE.getInstance().enabled) {
                 mc.options.useKey.setPressed(false);
                 updatedInventory = false;
             }
